6f6d1d6507245d8408793f684acaa61ea1aae925
Maruf Hasan <marufhasanmitul@gmail.com>
2023-06-19 02:00:56 +0600

    Module 7 Live test

--- lib/main.dart
+++ lib/main.dart
@@ -13,62 +13,9 @@ class myApp extends StatelessWidget {
       darkTheme: ThemeData(primarySwatch: Colors.cyan),
       color: Colors.green,
       debugShowCheckedModeBanner: false,
-      home: HomeActivity(),
+
     );
   }
 }
 
-class HomeActivity extends StatelessWidget {
 
-  List<String> shoppingItem=[
-    'Apple',
-    'Bananas',
-    'Bread',
-    'Milk',
-    'Eggs'
-  ];
-  MySnackbar(message,context){
-    ScaffoldMessenger.of(context).showSnackBar(
-        SnackBar(content: Text(message))
-    );
-
-  }
-
-
-
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('My Shopping List'),
-        backgroundColor: Colors.blue,
-        elevation: 10,
-        centerTitle:true,
-        actions: [
-          IconButton(
-              onPressed: (){
-                MySnackbar("Cart is empty", context);
-              },
-              icon: Icon(Icons.shopping_cart)),
-        ],
-
-      ),
-      body: ListView.builder(
-
-        itemCount:shoppingItem.length ,
-        itemBuilder: (BuildContext context, int index) {
-          return ListTile(
-            title: Text(shoppingItem[index]),
-            leading: Icon(Icons.shopping_basket),
-
-          );
-        },
-      ),
-
-
-    );
-
-
-  }
-}
